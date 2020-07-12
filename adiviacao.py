bb32118089f7bfc2164ff786e00a815e2549aae2
Bea Portella <blpportella@gmail.com>
2020-07-12 19:35:15 -0300

    Update adiviacao.py

--- adiviacao.py
+++ adiviacao.py
@@ -1,18 +1,26 @@
+import random
+
 print("********************************")
 print("Bem vindo ao jogo de advinhação!")
 print("********************************")
 
-numero_secreto = 42
+numero_secreto = round(random.random() * 100)
 total_de_tentativas = 3
-rodada = 1
+
+print(numero_secreto)
+
 #enquanto ainda há tentativas disponíveis: execute o código abaixo.
 #em cada laço será subtraido um do total de tentativas.
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     #saberemos a rodada atual
-    print("Tentativas", rodada, "de", total_de_tentativas)
+    print("Tentativas {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu número: "))
     print("Você digitou", chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100. Você perdeu uma rodada!")
+        continue
 
     acertou = numero_secreto == chute
     chute_foi_maior = chute > numero_secreto
